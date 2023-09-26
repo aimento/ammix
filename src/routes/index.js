0e9805d1c9a43ce0be012fc6ba565599f60ffd4f
Lee Sangwon <emicree@gmail.com>
2023-09-26 14:12:41 +0900

    서버 코드를 remix에 맞게 수정함.

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const routes = express.Router();
-const { userRouter } = require("./user.routes");
-
-routes.use("/user", userRouter);
-
-module.exports = { routes };
