d3973e0ed3edb42c362466c12cf7192edf6aa9da
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-10-13 13:34:10 +0900

    add : 로그아웃기능 구현

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,21 +1,35 @@
-import { LoaderFunction, useLoaderData } from "@remix-run/react";
-import { getSession } from "~/services/session.server";
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import {
+  LoaderFunction,
+  useLoaderData,
+  ActionFunction,
+} from "@remix-run/react";
+import { getSession, commitSession } from "~/services/session.server";
 import { Users } from "~/models/users.server";
 
+//세션에서 쿠키의 유저정보 찾기
 export let loader: LoaderFunction = async ({ request }) => {
   let session = await getSession(request.headers.get("cookie"));
   let userId = session.get("userdata")?.userId;
-  console.log("UserId from session: ", userId);
-  console.log("Session data: ", session.get("userdata"));
+  console.log(session);
 
   let user = null;
-
   if (userId) {
     user = await Users.findOne({ username: userId });
   }
 
   return json({ user });
+};
+
+//로그아웃하기
+export let action: ActionFunction = async ({ request }) => {
+  let session = await getSession(request.headers.get("cookie"));
+  session.unset("userdata");
+  let headers = new Headers({
+    "Set-Cookie": await commitSession(session),
+  });
+
+  return redirect("/", { headers });
 };
 
 export default function Home() {
@@ -26,11 +40,14 @@ export default function Home() {
     <div>
       {user ? (
         <div>
-          <p>Welcome, {user.username}</p>
+          <p>Welcome☺️, {user.username}</p>
+          <form method="post" action="">
+            <button type="submit">로그아웃</button>
+          </form>
         </div>
       ) : (
         <div>
-          <a href="/sign-in">Sign In</a>
+          <a href="/sign-in">로그인화면으로 가기!</a>
         </div>
       )}
     </div>
