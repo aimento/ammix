02cbd65e222fb19b4f0c6c523ca8999d14cb28b7
ParkGeonRyul <qyskamslepf@gmail.com>
2023-10-17 11:29:28 +0900

    feature : profile_image

--- app/routes/_my.profile/route.tsx
+++ app/routes/_my.profile/route.tsx
@@ -1,42 +1,55 @@
 import { redirect, json } from "@remix-run/node";
-import type {
-  LoaderFunctionArgs,
-  ActionFunctionArgs,
+import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
+import {
+  unstable_composeUploadHandlers as composeUploadHandlers,
+  unstable_createFileUploadHandler as createFileUploadHandler,
+  unstable_createMemoryUploadHandler as createMemoryUploadHandler,
+  unstable_parseMultipartFormData as parseMultipartFormData,
 } from "@remix-run/node";
-import { unstable_createFileUploadHandler } from "@remix-run/node";
 import { Users } from "../../models/users.server";
 import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import { commitSession, getSession } from "~/services/session.server";
 import { validateValue } from "~/utils/validator";
 import { sessionCommit } from "~/services/commit.server";
 import React, { useState, useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
 
 export async function action({ request }: ActionFunctionArgs) {
   const session = await getSession(request.headers.get("Cookie"));
 
   const sessionId = session.get(session.id);
-  const formData = await request.formData();
+  let formData = await request.formData();
 
   if (formData.get("_action") === "nameEdit") {
     const changeFirstName = formData.get("changeFirstName");
-    const changeFirstNameValidate = validateValue(changeFirstName, "change name");
+    const changeFirstNameValidate = validateValue(
+      changeFirstName,
+      "change name"
+    );
     const changeLastName = formData.get("changeLastName");
     const changeLastNameValidate = validateValue(changeLastName, "change name");
 
     if (changeFirstNameValidate !== true) {
       const { status } = changeFirstNameValidate;
-      const errors = { status: status, firstName: changeFirstName, lastName: changeLastName };
+      const errors = {
+        status: status,
+        firstName: changeFirstName,
+        lastName: changeLastName,
+      };
       return json({ errors });
     }
 
     if (changeLastNameValidate !== true) {
       const { status } = changeLastNameValidate;
-      const errors = { status: status, firstName: changeFirstName, lastName: changeLastName };
+      const errors = {
+        status: status,
+        firstName: changeFirstName,
+        lastName: changeLastName,
+      };
       return json({ errors });
     }
 
-    const now = new Date()
+    const now = new Date();
 
     await Users.updateOne(
       { _id: sessionId },
@@ -44,7 +57,7 @@ export async function action({ request }: ActionFunctionArgs) {
         $set: {
           "avatar.firstName": changeFirstName,
           "avatar.lastName": changeLastName,
-          "updatedAt": now
+          updatedAt: now,
         },
       }
     );
@@ -54,19 +67,31 @@ export async function action({ request }: ActionFunctionArgs) {
     return redirect("/profile", { headers });
   }
 
-  if (formData.get("_action") === "imageEdit") {
+    // const uploadHandler = composeUploadHandlers(
+    //   createFileUploadHandler({
+    //     directory: "./Desktop/public/uploads",
+    //     // maxPartSize: 30000,
+    //   }),
+    //   createMemoryUploadHandler()
+    // );
+    // const imageForm = await parseMultipartFormData(request, uploadHandler);
+    // const image = imageForm.get("img");
+    // if (!image || typeof image === "string") {
+    //   return json({ error: "something wrong", imgSrc: null });
+    // }
 
-    const changeImage = formData.get("changeImage");
+    // return json({ error: null, imgSrc: image.name });
 
-    await Users.updateOne(
-      { _id: sessionId },
-      {
-        $set: {
-          "avatar.imageUrl": changeImage,
-        },
-      }
-    );
-  }
+    // const changeImage = formData.get("changeImage");
+
+    //   await Users.updateOne(
+    //     { _id: sessionId },
+    //     {
+    //       $set: {
+    //         "avatar.imageUrl": changeImage,
+    //       },
+    //     }
+    //   );
 
   return null;
 }
@@ -98,7 +123,18 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function testing() {
   const data = useLoaderData<typeof loader>();
-  
+
+  console.log(data.data.userImage);
+
+  function onChange(f: any) {
+    let file = f.files;
+
+    if (!/\.(png|jpeg)$/i.test(file[0].name)) {
+      alert("업로드할 수 없는 파일입니다.\n\n현재 파일 : " + file[0].name);
+      file[0].name = ""; // 해당 태그의 파일이름값을 비워준다.
+    } else return;
+  }
+
   return (
     <div>
       <label>UserID</label>
@@ -110,13 +146,15 @@ export default function testing() {
           <br></br>
           <div>
             <input
-            type="text"
-            defaultValue={data?.data.userName.firstName}
-            name="changeFirstName"/>
+              type="text"
+              defaultValue={data?.data.userName.firstName}
+              name="changeFirstName"
+            />
             <input
-            type="text"
-            defaultValue={data?.data.userName.lastName}
-            name="changeLastName"/>
+              type="text"
+              defaultValue={data?.data.userName.lastName}
+              name="changeLastName"
+            />
           </div>
           <button type="submit" name="_action" value="nameEdit">
             edit
@@ -124,21 +162,27 @@ export default function testing() {
         </div>
       </form>
       <br></br>
-      <form method="POST">
+      <Form method="POST" encType="multipart/form-data" action="/upload">
         <div>
           <br></br>
           <label>UserImage</label>
           <input
             type="file"
-            name="changeImage"
-            // onSubmit={onSubmit}
+            name="img"
+            // onChange={onChange(this)}
           />
           <br></br>
         </div>
         <button type="submit" name="_action" value="imageEdit">
           edit
         </button>
-      </form>
+      </Form>
+      {data?.data.userImage? (
+        <div>
+          <h2>uploaded image</h2>
+          <img alt="uploaded" src={data.data.userImage} />
+        </div>
+      ) : null}
     </div>
   );
 }
