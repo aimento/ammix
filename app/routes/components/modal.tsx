0f7220646958c24806ae023ddc425e32a7717257
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-10-19 11:11:18 +0900

    fix: 버그픽스

--- app/routes/components/modal.tsx
+++ app/routes/components/modal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Modal({ onClose, actionUrl }) {
+function Modal({ onClose, onConfirm, message, showCancelButton = true }) {
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen">
@@ -12,20 +12,23 @@ function Modal({ onClose, actionUrl }) {
               className="text-lg leading-6 font-normal text-gray-900"
               id="modal-title"
             >
-              로그아웃 하시겠습니까? :(
+              {message}
             </h3>
           </div>
           <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button
-              onClick={onClose}
-              type="button"
-              className="w-full h-12 py-2 px-4 border border-gray-300 text-gray-700 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:w-1/2 sm:text-center sm:ml-3"
-            >
-              아니요
-            </button>
-            <form method="post" action={actionUrl} className="sm:w-1/2">
+            {showCancelButton && (
+              <button
+                onClick={onClose}
+                type="button"
+                className="w-full h-12 py-2 px-4 border border-gray-300 text-gray-700 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:w-1/2 sm:text-center sm:ml-3"
+              >
+                아니요
+              </button>
+            )}
+            <form method="post" className="sm:w-1/2">
               <button
                 type="submit"
+                onClick={onConfirm}
                 className="w-full h-12 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 text-center"
               >
                 예
